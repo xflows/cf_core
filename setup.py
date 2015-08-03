5eb3bb7e5c5f19f8c32770a40bafd1bf7cafbe9d
Darko Aleksovski <darko.aleksovski@ijs.si>
2015-08-03 16:10:43 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+import os
+from setuptools import setup
+
+setup(
+    name='cf_base',
+    version='0.1',
+    include_package_data=True,
+    license='MIT License',
+    description='Base package for ClowdFlows',
+    
+#    install_requires=[
+#          'pprint',
+#    ],
+    author='',
+    author_email='',
+)
